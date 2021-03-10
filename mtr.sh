ac4f39db6f87f4831c92cfc9611770226b7e6164
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2021-03-10 19:42:43 +0530

    Organise logs by date
    Logs will be in their respected date's directory

--- mtr.sh
+++ mtr.sh
@@ -4,7 +4,8 @@ set -o pipefail
 
 # Shell Script to do MTR for a server(s) specified in "servers.txt" file
 
-TIME=$(date "+%d%m%y_%H%M%S")
+DATE=$(date "+%d%m%y")
+TIME=$(date "+%H%M%S")
 
 function DO_MTR() {
     echo "MTR for ${1}"
@@ -28,7 +29,11 @@ if [[ ! -d "out" ]]; then
     mkdir out
 fi
 
+if [[ ! -d "out/${DATE}" ]]; then
+    mkdir -p out/"${DATE}"
+fi
+
 SERVERS="$(cat servers.txt)"
 for SERVER in $SERVERS; do
-    DO_MTR "${SERVER}" | tee -a out/"${TIME}".log
+    DO_MTR "${SERVER}" | tee -a out/"${DATE}"/"${TIME}".log
 done
