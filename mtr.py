9f502bcf0fbaa1712e8768451d896010b5cb5562
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2021-06-15 16:18:45 +0530

    Add mtr.py, and use it instead of writing markdown file with BASH for consistency
    Set 5 minutes as default time to run cron
    
    .env
    Dockerfile
    helper.sh
    mtr.py
    mtr.sh

---
+++ mtr.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+# Python Program to run MTR every X minutes
+
+__author__ = "Jugal Kishore"
+
+
+import sys
+from os.path import dirname
+import shlex
+import subprocess
+
+# Global Variable(s)
+PATH = sys.argv[0]
+__DIR = dirname(PATH)
+__SERVERS = f"{__DIR}/servers.txt"
+SERVERS = []
+
+# Read server(s) from "servers.txt" file
+print("Reading servers...")
+with open(__SERVERS, "r") as servers:
+    servers = servers.readlines()
+    for server in servers:
+        SERVERS.append(server.strip())
+
+
+def run_in_shell(COMMAND):
+    final_command = shlex.split(COMMAND)
+    PROCESSED = subprocess.run(final_command, capture_output=True)
+    return PROCESSED
+
+
+def bytes_to_string(BYTE):
+    return BYTE.decode("utf-8").strip()
+
+
+def do_mtr(SERVER):
+    command = f"mtr -wrzbc 3 {SERVER}"
+    PROCESSED = run_in_shell(command)
+    if PROCESSED.returncode == 0:
+        return bytes_to_string(PROCESSED.stdout)
+    else:
+        return ""
+
+
+TO_WRITE = ""
+for SERVER in SERVERS:
+    print("MTR for", SERVER)
+    print("---\n")
+    MTR = do_mtr(SERVER)
+    if not MTR:
+        pass
+    else:
+        print(MTR)
+        TO_WRITE += f"### MTR for {SERVER}\n"
+        TO_WRITE += "---\n\n"
+        TO_WRITE += f"```\n{MTR}\n```\n\n"
+    print("")
+
+with open("out.md", "w") as final:
+    final.write(TO_WRITE)
